a7cd9a3d6b4950403079b99aa372fa83dbfb1396
1234 <abc@gmail.com>
2025-04-02 15:23:02 -0500

    4/2/25 Abdulaziz A I added done, remove, undo, show funtion. I run a couple test to see if it works and it does.

--- Todolist.py
+++ Todolist.py
@@ -15,3 +15,34 @@ class ToDoList:
         
         else:
             print("Invalid index")
+            
+    def mark_done(self, index):
+        if 0 <= index < len(self.tasks):
+            self.tasks[index]['done'] = True
+            print(f'Task marked as done: {self.tasks[index]["task"]}')
+        else:
+            print("Invalid index")
+    
+    def undo_remove(self):
+        if self.history:
+            restored_task = self.history.pop()
+            self.tasks.append(restored_task)
+            print(f'Task restored: { restored_task["task"]}')
+        else:
+            print("No tasks to restore")
+        
+    def show_tasks(self):
+        if not self.tasks:
+            print('No tasks in the list')
+        for i, task in enumerate(self.tasks):
+            status = 'Done' if task['done'] else 'Pending'
+            print(f'{i}. {task["task"]} [{status}]')
+todo = ToDoList()
+todo.add_task("complete TODO list")
+todo.add_task("Python project")
+todo.show_tasks()
+todo.mark_done(0)
+todo.remove_task(1)
+todo.show_tasks()
+todo.undo_remove()
+todo.show_tasks()
